852dc7ca561c03dad9b6bea7f850e3c65d814381
Jakob Hansen <jakob.hansen@hotmail.no>
2022-12-17 19:03:05 +0100

    Solutions

--- 2022/11/src/main.rs
+++ 2022/11/src/main.rs
@@ -1,6 +1,13 @@
 use std::{error::Error, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // first()?;
+    second()?;
+
+    Ok(())
+}
+
+fn first() -> Result<(), Box<dyn Error>> {
     let input = io::read_to_string(io::stdin())?;
     let instructions: Vec<(i32, i32)> = input
         .lines()
@@ -11,13 +18,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             _ => unreachable!(),
         })
         .collect();
-
     let mut x = 1;
     let mut curr_cycle = 0;
     let mut res = 0;
     let mut next_checkpoint = 20;
-
-    for (i, (cycles, diff)) in instructions.iter().enumerate() {
+    for (cycles, diff) in instructions {
         curr_cycle += cycles;
         if curr_cycle >= next_checkpoint {
             res += x * next_checkpoint;
@@ -27,6 +32,43 @@ fn main() -> Result<(), Box<dyn Error>> {
         x += diff;
     }
     println!("{}", res);
+    Ok(())
+}
 
+fn second() -> Result<(), Box<dyn Error>> {
+    let input = io::read_to_string(io::stdin())?;
+    let instructions: Vec<(i32, i32)> = input
+        .lines()
+        .map(|line| line.split_once(" ").or(Some(("noop", ""))).unwrap())
+        .map(|(instruction, rest)| match instruction {
+            "addx" => (2, rest.parse::<i32>().unwrap()),
+            "noop" => (1, 0),
+            _ => unreachable!(),
+        })
+        .collect();
+    let mut x = 1;
+    let mut curr_cycle = 0;
+    let mut next_checkpoint = 40;
+    let mut screen: Vec<String> = vec![];
+    for (cycles, diff) in instructions {
+        for _ in 0..cycles.to_owned() {
+            let pixel_x = (curr_cycle + 40) - next_checkpoint;
+            println!("cycle {curr_cycle} x {} pixel_x {}", x, pixel_x);
+            if (x - 1) <= pixel_x && (x + 1) >= pixel_x {
+                screen.push(String::from("#"));
+            } else {
+                screen.push(String::from("."));
+            }
+            curr_cycle += 1;
+            if curr_cycle >= next_checkpoint {
+                next_checkpoint += 40;
+                println!();
+                screen.push(String::from("\n"));
+            }
+        }
+
+        x += diff;
+    }
+    println!("{}", screen.join(""));
     Ok(())
 }
