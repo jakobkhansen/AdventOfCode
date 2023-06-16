3740602a44811244eb1baf4688a2d642711ee73c
Jakob Hansen <jakob.hansen@hotmail.no>
2023-06-16 13:38:39 +0200

    Updates

--- 2022/10/src/main.rs
+++ 2022/10/src/main.rs
@@ -1,7 +1,74 @@
 use std::{error::Error, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = io::read_to_string(io::stdin())?;
+    // first()?;
+    second()?;
 
+    Ok(())
+}
+
+fn first() -> Result<(), Box<dyn Error>> {
+    let input = io::read_to_string(io::stdin())?;
+    let instructions: Vec<(i32, i32)> = input
+        .lines()
+        .map(|line| line.split_once(" ").or(Some(("noop", ""))).unwrap())
+        .map(|(instruction, rest)| match instruction {
+            "addx" => (2, rest.parse::<i32>().unwrap()),
+            "noop" => (1, 0),
+            _ => unreachable!(),
+        })
+        .collect();
+    let mut x = 1;
+    let mut curr_cycle = 0;
+    let mut res = 0;
+    let mut next_checkpoint = 20;
+    for (cycles, diff) in instructions {
+        curr_cycle += cycles;
+        if curr_cycle >= next_checkpoint {
+            res += x * next_checkpoint;
+            next_checkpoint += 40;
+        }
+
+        x += diff;
+    }
+    println!("{}", res);
+    Ok(())
+}
+
+fn second() -> Result<(), Box<dyn Error>> {
+    let input = io::read_to_string(io::stdin())?;
+    let instructions: Vec<(i32, i32)> = input
+        .lines()
+        .map(|line| line.split_once(" ").or(Some(("noop", ""))).unwrap())
+        .map(|(instruction, rest)| match instruction {
+            "addx" => (2, rest.parse::<i32>().unwrap()),
+            "noop" => (1, 0),
+            _ => unreachable!(),
+        })
+        .collect();
+    let mut x = 1;
+    let mut curr_cycle = 0;
+    let mut next_checkpoint = 40;
+    let mut screen: Vec<String> = vec![];
+    for (cycles, diff) in instructions {
+        for _ in 0..cycles.to_owned() {
+            let pixel_x = (curr_cycle + 40) - next_checkpoint;
+            println!("cycle {curr_cycle} x {} pixel_x {}", x, pixel_x);
+            if (x - 1) <= pixel_x && (x + 1) >= pixel_x {
+                screen.push(String::from("#"));
+            } else {
+                screen.push(String::from("."));
+            }
+            curr_cycle += 1;
+            if curr_cycle >= next_checkpoint {
+                next_checkpoint += 40;
+                println!();
+                screen.push(String::from("\n"));
+            }
+        }
+
+        x += diff;
+    }
+    println!("{}", screen.join(""));
     Ok(())
 }
--- 2022/11/src/main.rs
+++ 2022/11/src/main.rs
@@ -1,74 +1,14 @@
 use std::{error::Error, io};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // first()?;
-    second()?;
-
-    Ok(())
+fn main() {
+    let input = io::read_to_string(io::stdin()).unwrap();
+    println!("{}", first(&input));
+    println!("{}", second(&input));
 }
 
-fn first() -> Result<(), Box<dyn Error>> {
-    let input = io::read_to_string(io::stdin())?;
-    let instructions: Vec<(i32, i32)> = input
-        .lines()
-        .map(|line| line.split_once(" ").or(Some(("noop", ""))).unwrap())
-        .map(|(instruction, rest)| match instruction {
-            "addx" => (2, rest.parse::<i32>().unwrap()),
-            "noop" => (1, 0),
-            _ => unreachable!(),
-        })
-        .collect();
-    let mut x = 1;
-    let mut curr_cycle = 0;
-    let mut res = 0;
-    let mut next_checkpoint = 20;
-    for (cycles, diff) in instructions {
-        curr_cycle += cycles;
-        if curr_cycle >= next_checkpoint {
-            res += x * next_checkpoint;
-            next_checkpoint += 40;
-        }
-
-        x += diff;
-    }
-    println!("{}", res);
-    Ok(())
+fn first(input: &String) -> i32 {
+    0
 }
-
-fn second() -> Result<(), Box<dyn Error>> {
-    let input = io::read_to_string(io::stdin())?;
-    let instructions: Vec<(i32, i32)> = input
-        .lines()
-        .map(|line| line.split_once(" ").or(Some(("noop", ""))).unwrap())
-        .map(|(instruction, rest)| match instruction {
-            "addx" => (2, rest.parse::<i32>().unwrap()),
-            "noop" => (1, 0),
-            _ => unreachable!(),
-        })
-        .collect();
-    let mut x = 1;
-    let mut curr_cycle = 0;
-    let mut next_checkpoint = 40;
-    let mut screen: Vec<String> = vec![];
-    for (cycles, diff) in instructions {
-        for _ in 0..cycles.to_owned() {
-            let pixel_x = (curr_cycle + 40) - next_checkpoint;
-            println!("cycle {curr_cycle} x {} pixel_x {}", x, pixel_x);
-            if (x - 1) <= pixel_x && (x + 1) >= pixel_x {
-                screen.push(String::from("#"));
-            } else {
-                screen.push(String::from("."));
-            }
-            curr_cycle += 1;
-            if curr_cycle >= next_checkpoint {
-                next_checkpoint += 40;
-                println!();
-                screen.push(String::from("\n"));
-            }
-        }
-
-        x += diff;
-    }
-    println!("{}", screen.join(""));
-    Ok(())
+fn second(input: &String) -> i32 {
+    0
 }
